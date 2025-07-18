73520de78acbd134956ef829cdf468fad340b373
Jayliste <jaylisteledama94@gmail.com>
2025-07-18 12:44:13 +0300

    [UPDATE] added header

--- st_workorder_sequence/__manifest__.py
+++ st_workorder_sequence/__manifest__.py
@@ -13,5 +13,5 @@
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
-    'images': ['static/description/icon.png'],
+    'images': ['static/description/images/header.png'],
 }
